e2cedbd71d0dc2a00d320867114e6f20239818fa
Nvard <nvard.asatryan96@gmail.com>
2021-05-22 16:39:45 +0500

    header function

--- js/script.js
+++ js/script.js
@@ -9,4 +9,26 @@ $('.toggle').click(function(){
             
  
   });
+  
+  function header(){
+	   if( window.scrollY > 60 ) {
+              
+			$('.header').addClass('fixed');
+			$('.bt-hide').css({'display':'none'});
+			$('.language').css({'display':'none'});
+			$('.btn__free').css({'display':'block'});
 
+
+              
+            } else{
+				$('.header').removeClass('fixed')
+				$('.bt-hide').css({'display':'block'});
+				$('.language').css({'display':'block'});
+				$('.btn__free').css({'display':'none'});
+
+
+			}
+  }
+
+
+ window.addEventListener('scroll',header);
